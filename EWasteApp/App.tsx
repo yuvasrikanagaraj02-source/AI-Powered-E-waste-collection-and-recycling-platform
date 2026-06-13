fabbb17dc80e637733decc7126e99cd80f564d0d
Yuvasri_K <yuvasrikanagaraj02@gmail.com>
2026-06-13 20:23:08 +0530

    Add files via upload

---
+++ EWasteApp/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
+import { AuthProvider } from './src/context/AuthContext';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="light" />
+      <AuthProvider>
+        <AppNavigator />
+      </AuthProvider>
+    </SafeAreaProvider>
+  );
+}
